8cc2c431157ca792cdab1c42543d38c6ae87a1d7
Michael Bonner <michaelwbonner@gmail.com>
2021-08-24 18:44:59 -0600

    video tiles on hover

--- web/pages/index.js
+++ web/pages/index.js
@@ -6,46 +6,66 @@ import Link from "next/link";
 import { useEffect, useState } from "react";
 import useWindowSize from "../hooks/useWindowSize";
 
-function Home({ workItems }) {
+const WorkItemTile = ({ workItem }) => {
+  const [isHovered, setIsHovered] = useState(false);
+  return (
+    <Link href={`/work/${workItem.slug.current}`} key={workItem._id}>
+      <a
+        className="relative text-white flex flex-col items-center justify-center space-y-2 lg:space-y-0 bpd-project-tile"
+        key={workItem._id}
+        style={{
+          backgroundImage: `url(${urlForSanitySource(workItem.poster)})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+        }}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
+        {workItem.shortClipMp4URL && workItem.shortClipOgvURL && (
+          <video
+            className={`absolute inset-0 object-cover transition-all duration-700 ${
+              isHovered ? "opacity-100" : "opacity-0"
+            }`}
+            muted
+            autoPlay
+            loop
+          >
+            <source id="mp4" src={workItem.shortClipMp4URL} type="video/mp4" />
+            <source id="ogv" src={workItem.shortClipOgvURL} type="video/ogg" />
+          </video>
+        )}
+        <div className="z-10">
+          <h2 className="uppercase font-extrabold text-3xl lg:text-xl">
+            {workItem.clientName}
+          </h2>
+          <h3 className="uppercase font-outline text-2xl lg:text-lg">
+            {workItem.title}
+          </h3>
+        </div>
+      </a>
+    </Link>
+  );
+};
+
+function Home({ homePage, workItems }) {
   const [isDesktop, setIsDesktop] = useState(false);
   const size = useWindowSize();
-
-  const heroImageUrl = workItems.length > 0 ? workItems[0].poster : null;
 
   useEffect(() => {
     setIsDesktop(size.width >= 1024);
   }, [size.width]);
 
   return (
-    <Layout heroImageUrl={heroImageUrl} isDesktop={isDesktop}>
+    <Layout
+      title={homePage.seo_title}
+      description={homePage.seo_description}
+      heroImageUrl={homePage.poster}
+      isDesktop={isDesktop}
+    >
       <div className="mx-auto lg:grid lg:grid-cols-3">
         {workItems.map((workItem, index) => {
-          if (isDesktop && index === 0) {
-            return "";
-          }
-          return (
-            <Link href={`/work/${workItem.slug.current}`} key={workItem._id}>
-              <a
-                className="text-white flex flex-col items-center justify-center space-y-2 lg:space-y-0 bpd-project-tile"
-                key={workItem._id}
-                style={{
-                  backgroundImage: `url(${urlForSanitySource(
-                    workItem.poster
-                  )})`,
-                  backgroundSize: "cover",
-                  backgroundPosition: "center",
-                  backgroundRepeat: "no-repeat",
-                }}
-              >
-                <h2 className="uppercase font-extrabold text-3xl lg:text-xl">
-                  {workItem.clientName}
-                </h2>
-                <h3 className="uppercase font-outline text-2xl lg:text-lg">
-                  {workItem.title}
-                </h3>
-              </a>
-            </Link>
-          );
+          return <WorkItemTile workItem={workItem} key={index} />;
         })}
       </div>
     </Layout>
@@ -53,13 +73,32 @@ function Home({ workItems }) {
 }
 
 export async function getStaticProps() {
+  const homePage = await getClient().fetch(
+    groq`
+  *[_type == "homePage"][0]{
+    seo_title,
+    seo_description,
+    poster,
+    video_id,
+  }
+  `
+  );
   const workItems = await getClient().fetch(
     groq`
-    *[_type == "workItem"][!(_id in path('drafts.**'))]|order(order asc)
+    *[_type == "workItem"][!(_id in path('drafts.**'))]|order(order asc){
+      _id,
+      slug,
+      clientName,
+      title,
+      poster,
+      "shortClipMp4URL": shortClipMp4.asset->url,
+      "shortClipOgvURL": shortClipOgv.asset->url,
+    }
   `
   );
   return {
     props: {
+      homePage,
       workItems,
     },
   };
