5a8b5e00f32ca35e6c8f0bed02e689f559c9d4ec
Michael Bonner <michaelwbonner@gmail.com>
2021-08-26 20:22:11 -0600

    add gtm

--- web/pages/_app.js
+++ web/pages/_app.js
@@ -1,6 +1,12 @@
 import "../styles/globals.css";
+import TagManager from "react-gtm-module";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    TagManager.initialize({ gtmId: "GTM-NQB4SLF" });
+  }, []);
+
   return <Component {...pageProps} />;
 }
 
