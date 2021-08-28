6be83c69141400a99828e5781dd01c6aee979a49
Michael Bonner <michaelwbonner@gmail.com>
2021-08-28 14:10:49 -0600

    use img instead of next image

--- web/pages/contact.js
+++ web/pages/contact.js
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import groq from "groq";
 import Image from "next/image";
 import BlockContent from "@sanity/block-content-to-react";
@@ -13,11 +14,9 @@ function Home({ about }) {
     >
       <div className="max-w-5xl mx-auto px-4 lg:px-0">
         <div className="relative border-2 border-black py-8 px-7 max-w-xs mx-auto w-full">
-          <Image
+          <img
             src={urlForSanitySource(about.photo).width(500).url()}
             alt="Jeremy Miller Headshot"
-            width="500"
-            height="621"
           />
           <div className="absolute px-4 w-full left-0 right-0 -bottom-6">
             <div className="flex items-center justify-center w-full">
