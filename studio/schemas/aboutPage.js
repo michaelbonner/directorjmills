6d6674ff13f8190868da1321aa074803424bbf38
Michael Bonner <michaelwbonner@gmail.com>
2021-08-29 21:17:59 -0600

    add max width for bio pic

--- studio/schemas/aboutPage.js
+++ studio/schemas/aboutPage.js
@@ -44,6 +44,52 @@ export default {
       type: "blockContent",
     },
     {
+      name: "bioMaxWidth",
+      title: "Bio Image Max Width",
+      type: "string",
+      description: "What is the maximum width of the image",
+      options: {
+        list: [
+          {
+            title: "xs",
+            value: "xs",
+          },
+          {
+            title: "sm",
+            value: "sm",
+          },
+          {
+            title: "md",
+            value: "md",
+          },
+          {
+            title: "lg",
+            value: "lg",
+          },
+          {
+            title: "xl",
+            value: "xl",
+          },
+          {
+            title: "2xl",
+            value: "2xl",
+          },
+          {
+            title: "3xl",
+            value: "3xl",
+          },
+          {
+            title: "4xl",
+            value: "4xl",
+          },
+          {
+            title: "5xl",
+            value: "5xl",
+          },
+        ],
+      },
+    },
+    {
       name: "representation",
       title: "Representation",
       type: "blockContent",
