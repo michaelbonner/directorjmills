6db8e540fd226a93462480edc9b7ef342a7d336e
Michael Bonner <michaelwbonner@gmail.com>
2021-09-24 11:04:48 -0600

    more redirects

--- web/next.config.js
+++ web/next.config.js
@@ -10,6 +10,36 @@ module.exports = {
         destination: "/work/webb-who-we-are",
         permanent: true,
       },
+      {
+        source: "/work/ACCELERATE",
+        destination: "/work/pattern-accelerate",
+        permanent: true,
+      },
+      {
+        source: "/work/UMMC",
+        destination: "/work/ummc-here-in-mississippi",
+        permanent: true,
+      },
+      {
+        source: "/work/tesla",
+        destination: "/work/tesla-model-x",
+        permanent: true,
+      },
+      {
+        source: "/work/TRAEGER",
+        destination: "/work/traeger-grills-d2",
+        permanent: true,
+      },
+      {
+        source: "/work/VERIZON",
+        destination: "/work/verizon",
+        permanent: true,
+      },
+      {
+        source: "/work/ACCELERATE",
+        destination: "/work/pattern-accelerate",
+        permanent: true,
+      },
     ];
   },
 };
