c4aaf13863aadfda1775a7dc29095c0c75b5a50d
Michael Bonner <michaelwbonner@gmail.com>
2021-09-24 10:55:26 -0600

    add redirect for web work item

--- web/next.config.js
+++ web/next.config.js
@@ -3,4 +3,13 @@ module.exports = {
   images: {
     domains: ["cdn.sanity.io"],
   },
+  async redirects() {
+    return [
+      {
+        source: "/work/webb",
+        destination: "/work/webb-who-we-are",
+        permanent: true,
+      },
+    ];
+  },
 };
