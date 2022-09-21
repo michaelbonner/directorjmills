08372f77a98d02b33a50c1b33247faae14046778
Michael Bonner <michaelwbonner@gmail.com>
2022-09-21 10:31:35 -0600

    --ammend

---
+++ web/data/menu-items.js
@@ -0,0 +1,17 @@
+export const menuItems = [
+  {
+    title: "Home",
+    href: "/",
+    hideOnMobile: false,
+  },
+  {
+    title: "Work",
+    href: "/work",
+    hideOnMobile: true,
+  },
+  {
+    title: "Contact",
+    href: "/contact",
+    hideOnMobile: false,
+  },
+];
