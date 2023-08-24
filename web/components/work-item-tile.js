913296dafe62a01f83960ac09cf1f5784dd03bce
Colton Bonner <cbonner26118@gmail.com>
2023-08-23 18:07:07 -0600

    Removed child anchor tags and moved style/animations into Links. Added 2.33 aspect ratio + unset same as JME.

--- web/components/work-item-tile.js
+++ web/components/work-item-tile.js
@@ -7,55 +7,54 @@ const WorkItemTile = ({ workItem, index, hideAfterCount = 999 }) => {
   const [hasHovered, setHasHovered] = useState(false);
 
   return (
-    <Link href={`/work/${workItem.slug.current}`} key={workItem._id}>
-      <a
-        className={`${
-          index >= hideAfterCount ? `lg:hidden` : null
-        } relative text-white flex flex-col items-center justify-center space-y-2 lg:space-y-0 bpd-project-tile`}
-        key={workItem._id}
-        style={{
-          backgroundImage: workItem.poster
-            ? `url(${urlForSanitySource(workItem.poster).width(700)})`
-            : "",
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          backgroundRepeat: "no-repeat",
-        }}
-        onMouseEnter={() => {
-          setHasHovered(true);
-          setIsHovered(true);
-        }}
-        onMouseLeave={() => setIsHovered(false)}
-        onTouchStart={() => {
-          setHasHovered(true);
-          setIsHovered(true);
-        }}
-        onTouchEnd={() => setIsHovered(false)}
-      >
-        {hasHovered && workItem.shortClipMp4URL && workItem.shortClipOgvURL && (
-          <video
-            className={`absolute w-full h-full inset-0 object-cover transition-all duration-700 ${
-              isHovered ? "opacity-100" : "opacity-0"
-            }`}
-            muted={true}
-            autoPlay={true}
-            loop={true}
-            playsInline={true}
-            preload="none"
-          >
-            <source id="mp4" src={workItem.shortClipMp4URL} type="video/mp4" />
-            <source id="ogv" src={workItem.shortClipOgvURL} type="video/ogg" />
-          </video>
-        )}
-        <div className="z-10 text-center">
-          <h2 className="uppercase font-extrabold text-3xl lg:text-2xl">
-            {workItem.clientName}
-          </h2>
-          <h3 className="uppercase font-outline text-2xl lg:text-xl">
-            {workItem.title}
-          </h3>
-        </div>
-      </a>
+    <Link
+      href={`/work/${workItem.slug.current}`}
+      key={workItem._id}
+      className={`${
+        index >= hideAfterCount ? `lg:hidden` : null
+      } relative text-white flex flex-col items-center justify-center space-y-2 lg:space-y-0 bpd-project-tile`}
+      style={{
+        backgroundImage: workItem.poster
+          ? `url(${urlForSanitySource(workItem.poster).width(700)})`
+          : "",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+      onMouseEnter={() => {
+        setHasHovered(true);
+        setIsHovered(true);
+      }}
+      onMouseLeave={() => setIsHovered(false)}
+      onTouchStart={() => {
+        setHasHovered(true);
+        setIsHovered(true);
+      }}
+      onTouchEnd={() => setIsHovered(false)}
+    >
+      {hasHovered && workItem.shortClipMp4URL && workItem.shortClipOgvURL && (
+        <video
+          className={`absolute w-full h-full inset-0 object-cover transition-all duration-700 ${
+            isHovered ? "opacity-100" : "opacity-0"
+          }`}
+          muted={true}
+          autoPlay={true}
+          loop={true}
+          playsInline={true}
+          preload="none"
+        >
+          <source id="mp4" src={workItem.shortClipMp4URL} type="video/mp4" />
+          <source id="ogv" src={workItem.shortClipOgvURL} type="video/ogg" />
+        </video>
+      )}
+      <div className="z-10 text-center">
+        <h2 className="uppercase font-extrabold text-3xl lg:text-2xl">
+          {workItem.clientName}
+        </h2>
+        <h3 className="uppercase font-outline text-2xl lg:text-xl">
+          {workItem.title}
+        </h3>
+      </div>
     </Link>
   );
 };
